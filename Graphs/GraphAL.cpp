f1927323f2362f462b6084e268b148a81fa3a948
Emanuel67E <120481014+Emanuel67E@users.noreply.github.com>
2025-08-27 15:01:26 -0400

    Update GraphAL.cpp

--- Graphs/GraphAL.cpp
+++ Graphs/GraphAL.cpp
@@ -14,6 +14,9 @@ public:
   GraphAL(uint);
   ~GraphAL();
   void add_edge(Vertex, Vertex);
+  void remove_edge(Vertex, Vertex);
+  std::list<Vertex> get_adj(Vertex);
+  uint get_num_vertices() { return num_vertices;}
 };
 
 GraphAL::GraphAL(uint _num_vertices) {
@@ -24,7 +27,7 @@ GraphAL::GraphAL(uint _num_vertices) {
 
 GraphAL::~GraphAL() {
   delete[] adj;
-  adj = nullptr;
+ adj = nullptr;
 }
 
 void GraphAL::add_edge(Vertex u, Vertex v) {
@@ -33,6 +36,34 @@ void GraphAL::add_edge(Vertex u, Vertex v) {
   }
   adj[u].push_back(v);
   adj[v].push_back(u);
+  num_edges++;
+}
+
+void GraphAL::remove_edge(Vertex u, Vertex v) {
+  if(u == v || u >= num_vertices || v >= num_vertices) {
+    throw std::runtime_error("Entradas invalidas!\n");
+  }
+  adj[u].remove(v);
+  adj[v].remove(u);
+  num_edges--;
+}
+
+std::list<Vertex> GraphAL::get_adj(Vertex u) {
+  if(u >= num_vertices) {
+    throw std::runtime_error("Entrada invalida!\n");
+  }
+  return adj[u];
+}
+
+void print_adjacency_list(GraphAL g) {
+  for(int u=0; u<g.get_num_vertices(); u++) {
+    std::list<Vertex> adj = g.get_adj(u);
+    std::cout << u << ": ";
+    for(auto e: adj) {
+      std::cout << e << ", ";
+    }
+    std::cout << std::endl;
+  }
 }
 
 int main() 
